326a63859106e47b61dd1d3456fec756c6b65caa
completejavascript <completejavascript.super@gmail.com>
2019-10-07 00:48:00 +0700

    Implement zoom slider

--- src/components/ImageCrop/ImageCrop.js
+++ src/components/ImageCrop/ImageCrop.js
@@ -1,12 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Cropper from 'react-easy-crop';
-import { Button } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Slider from '@material-ui/core/Slider';
+import Input from '@material-ui/core/Input';
+import { withStyles } from '@material-ui/core/styles';
 import { createImage, getCroppedImg } from '../../utils/ImageHandler';
 import './ImageCrop.scss';
 
 const LOADED_IMAGE_CONTAINER_SIZE = 480;
 const PREVIEW_CONTAINER_SIZE = 288;
+
+const PrettoSlider = withStyles({
+  root: {
+    color: '#5665d7',
+    height: 4,
+    width: '160px',
+    marginLeft: '15px',
+    marginRight: '15px'
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: 'currentColor',
+    border: '2px solid currentColor',
+    marginTop: -10,
+    marginLeft: -12,
+    '&:focus,&:hover,&$active': {
+      boxShadow: 'inherit'
+    }
+  },
+  track: {
+    height: 4,
+    borderRadius: 4
+  },
+  rail: {
+    height: 4,
+    borderRadius: 4
+  },
+  active: {}
+})(Slider);
 
 const Title = ({ text, width, height }) => (
   <div className="title-wrapper">
@@ -58,6 +91,63 @@ const PreviewImage = ({
   );
 };
 
+const ZoomSection = ({
+  zoom,
+  minZoom,
+  maxZoom,
+  adaptZoomBase,
+  handleZoomChange
+}) => {
+  const [percentage, setPercentage] = useState(
+    Math.round((zoom * 100) / adaptZoomBase)
+  );
+
+  const handleOnChange = event => {
+    const newZoom = (Number(event.target.value) * adaptZoomBase) / 100;
+    handleZoomChange('', newZoom);
+  };
+
+  useEffect(() => {
+    setPercentage(Math.round((zoom * 100) / adaptZoomBase));
+  }, [zoom, adaptZoomBase]);
+
+  return (
+    <div className="zoom-section">
+      <span className="title">Zoom</span>
+      <PrettoSlider
+        value={zoom}
+        min={minZoom}
+        max={maxZoom}
+        step={0.1}
+        aria-labelledby="Zoom"
+        onChange={handleZoomChange}
+      />
+      <Input
+        className="percentage-input"
+        type="number"
+        value={percentage}
+        onChange={handleOnChange}
+      />
+      <span className="value">%</span>
+    </div>
+  );
+};
+
+const SaveSection = ({ handleSave }) => {
+  return (
+    <div className="btn-save-wrapper">
+      <Button
+        className="btn-save"
+        onClick={handleSave}
+        variant="contained"
+        color="primary"
+      >
+        Save
+      </Button>
+    </div>
+  );
+};
+
 const ImageCrop = props => {
   const { toWidth, toHeight, image, onCrop, minZoom, maxZoom } = props;
   const cropSize = {
@@ -66,7 +156,6 @@ const ImageCrop = props => {
   };
   const largerPreviewSize =
     toWidth > PREVIEW_CONTAINER_SIZE || toHeight > PREVIEW_CONTAINER_SIZE;
-  let adaptZoomBase = 1;
 
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
@@ -74,9 +163,9 @@ const ImageCrop = props => {
   const [aspect] = useState(toWidth / toHeight);
   const [loadedSize, setLoadedSize] = useState({ width: 0, height: 0 });
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
+  const [adaptZoomBase, setAdaptZoomBase] = useState(1);
 
   const onImageLoaded = imageSize => {
-    console.log('onImageLoaded', imageSize);
     const { naturalWidth, naturalHeight } = imageSize;
 
     if (naturalWidth < toWidth || naturalHeight < toHeight) {
@@ -85,25 +174,32 @@ const ImageCrop = props => {
       );
     }
 
-    adaptZoomBase = Math.max(
-      imageSize.naturalWidth / LOADED_IMAGE_CONTAINER_SIZE,
-      imageSize.naturalHeight / LOADED_IMAGE_CONTAINER_SIZE
-    );
-
-    if (adaptZoomBase > 1) {
-      setZoom(adaptZoomBase);
-    } else {
-      adaptZoomBase = 1;
+    if (
+      imageSize.naturalWidth > LOADED_IMAGE_CONTAINER_SIZE ||
+      imageSize.naturalHeight > LOADED_IMAGE_CONTAINER_SIZE
+    ) {
+      const zoomValue = Math.max(
+        imageSize.naturalWidth / LOADED_IMAGE_CONTAINER_SIZE,
+        imageSize.naturalHeight / LOADED_IMAGE_CONTAINER_SIZE
+      );
+      setZoom(zoomValue);
+      setAdaptZoomBase(zoomValue);
     }
   };
 
   const onCropComplete = (newCroppedArea, newCroppedAreaPixels) => {
-    console.log('onCropComplete', newCroppedArea, newCroppedAreaPixels, zoom);
+    console.log(
+      'onCropComplete',
+      newCroppedArea,
+      newCroppedAreaPixels,
+      zoom,
+      adaptZoomBase
+    );
     setCroppedAreaPixels(newCroppedAreaPixels);
   };
 
   const getCroppedImageToPreview = async () => {
-    if (image) {
+    if (image && croppedAreaPixels) {
       const croppedImage = await getCroppedImg(
         image,
         croppedAreaPixels,
@@ -124,6 +220,10 @@ const ImageCrop = props => {
     } catch (error) {
       console.error('Error to image size: ', image, error);
     }
+  };
+
+  const handleZoomChange = (_, zoom) => {
+    setZoom(zoom);
   };
 
   const handleSave = async () => {
@@ -170,31 +270,33 @@ const ImageCrop = props => {
 
       <div className="preview">
         {croppedAreaPixels && (
-          <Title
-            text="Preview"
-            width={croppedAreaPixels.width}
-            height={croppedAreaPixels.height}
-          />
+          <div className="preview-title-wrapper">
+            <Title
+              text="Preview"
+              width={croppedAreaPixels.width}
+              height={croppedAreaPixels.height}
+            />
+          </div>
         )}
         <div className="preview-container">
-          <PreviewImage
-            largerPreviewSize={largerPreviewSize}
-            croppedAreaPixels={croppedAreaPixels}
-            toWidth={toWidth}
-            toHeight={toHeight}
-            image={image}
-          />
-          <div className="controls">
-            <div className="btn-save-wrapper">
-              <Button
-                className="btn-save"
-                onClick={handleSave}
-                variant="contained"
-                color="primary"
-              >
-                Save
-              </Button>
-            </div>
+          <div className="top">
+            <PreviewImage
+              largerPreviewSize={largerPreviewSize}
+              croppedAreaPixels={croppedAreaPixels}
+              toWidth={toWidth}
+              toHeight={toHeight}
+              image={image}
+            />
+            <ZoomSection
+              zoom={zoom}
+              minZoom={minZoom}
+              maxZoom={maxZoom}
+              adaptZoomBase={adaptZoomBase}
+              handleZoomChange={handleZoomChange}
+            />
+          </div>
+          <div className="bottom">
+            <SaveSection handleSave={handleSave} />
           </div>
         </div>
       </div>
